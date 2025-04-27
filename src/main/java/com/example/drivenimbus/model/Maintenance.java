8d9c11f24bf4bb707ca4576f1bcf1ea6c01d3e68
ChrisK1504 <krismehilli@yahoo.com>
2025-04-27 19:50:56 +0200

    Created a model for a Maintenance entity;

---
+++ src/main/java/com/example/drivenimbus/model/Maintenance.java
@@ -0,0 +1,74 @@
+package com.example.drivenimbus.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+enum MaintanceStatus {
+    SCHEDULED,
+    COMPLETED
+}
+
+@Entity
+public class Maintenance {
+    @Id
+    private Long MaintenanceID;
+    private Date StartDate;
+    private Date EndDate;
+    private MaintanceStatus Status;
+    private String Description;
+
+    // Default constructor
+    public Maintenance() {
+    }
+
+    // Constructor with all fields
+    public Maintenance(Long maintenanceID, Date startDate, Date endDate, MaintanceStatus status, String description) {
+        MaintenanceID = maintenanceID;
+        StartDate = startDate;
+        EndDate = endDate;
+        Status = status;
+        Description = description;
+    }
+
+    public Long getMaintenanceID() {
+        return MaintenanceID;
+    }
+
+    public void setMaintenanceID(Long maintenanceID) {
+        MaintenanceID = maintenanceID;
+    }
+
+    public Date getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        StartDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        EndDate = endDate;
+    }
+
+    public MaintanceStatus getStatus() {
+        return Status;
+    }
+
+    public void setStatus(MaintanceStatus status) {
+        Status = status;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
