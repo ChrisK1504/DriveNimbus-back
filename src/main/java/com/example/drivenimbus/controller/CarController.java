85b5968e4282aedef60279e746eb8a7df8f5be77
ChrisK1504 <krismehilli@yahoo.com>
2025-04-29 13:36:31 +0200

    Implemented a Car controller; TODO - Write unit tests for each operation;

--- src/main/java/com/example/drivenimbus/controller/CarController.java
+++ src/main/java/com/example/drivenimbus/controller/CarController.java
@@ -1,14 +1,46 @@
 package com.example.drivenimbus.controller;
 
 import com.example.drivenimbus.model.Car;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.drivenimbus.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/cars")
 public class CarController {
+    @Autowired private CarService carService;
 
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.fetchCarList();
+    }
 
+    @GetMapping("/{carId}")
+    public ResponseEntity<Car> getCarById(@PathVariable Long carId) {
+        Car car = carService.fetchCarById(carId);
+        return car != null ? ResponseEntity.ok(car) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{carId}")
+    public ResponseEntity<Void> deleteCarById(@PathVariable Long carId) {
+        if (carService.deleteCarById(carId)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Car saveCar(@RequestBody Car car) {
+        return carService.saveCar(car);
+    }
+
+    @PutMapping("/{carId}")
+    public ResponseEntity<Car> updateCar(@RequestBody Car updatedCar, @PathVariable Long carId) {
+        Car car = carService.updateCar(updatedCar, carId);
+        return car != null ? ResponseEntity.ok(car) : ResponseEntity.notFound().build();
+    }
 
 }
